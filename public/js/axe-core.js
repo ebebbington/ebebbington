341a8c4e1f02790afc0d5fbd34b7448fcc064b00
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-05 15:15:51 +0100

    update axe-core file

--- public/js/axe-core.js
+++ public/js/axe-core.js
@@ -2,16 +2,18 @@
  * `npm i axe-core
  * Inject: <script src="node_modules/axe-core/axe.min.js"></script> in the html file
  */
-axe
-  .run()
-  .then(results => {
-    if (results.violations.length) {
-      console.error("Accessibility issues found:")
-      console.error(results.violations)
-    } else {
-      console.log('No accessibility issues found')
-    }
-  })
-  .catch(err => {
-    console.error('Something bad happened:', err.message);
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  axe
+    .run()
+    .then(results => {
+      if (results.violations.length) {
+        console.error("Accessibility issues found:")
+        console.error(results.violations)
+      } else {
+        console.log('No accessibility issues found')
+      }
+    })
+    .catch(err => {
+      console.error('Something bad happened:', err.message);
+    });
+})
