a1b82ab5bc409c35ff78b5851f4ac2e9615b2349
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2021-09-16 20:31:32 +0100

    fix ci (#8)
    * fix ci
    
    * remove logging

--- make_website_a_server.ts
+++ make_website_a_server.ts
@@ -3,16 +3,26 @@ import { Drash } from "./deps.ts"
 class HomeResource extends Drash.Http.Resource {
   static paths = ["/"]
   public GET () {
-    this.response.body = this.response.render("/index.html")
+    this.response.body = Deno.readTextFileSync("./index.html")
+    return this.response
+  }
+}
+
+class FilesResource extends Drash.Http.Resource {
+  static paths = ["/public/:dir/:file"]
+
+  public GET() {
+    const { url } = this.request
+    const mimeType = url.endsWith(".css") ? "text/css" : url.endsWith(".js") ? "application/javascript" : "image/png"
+    this.response.body = Deno.readFileSync("." + this.request.url)
+    this.response.headers.set("Content-Type", mimeType)
     return this.response
   }
 }
 
 const server = new Drash.Http.Server({
   directory: ".",
-  resources: [HomeResource],
-  views_path: ".",
-  static_paths: ["/public"]
+  resources: [HomeResource, FilesResource],
 })
 
 await server.run({
